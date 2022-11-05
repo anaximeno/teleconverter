2a5fa94cda20be87ee259d9f2b9138b7ac1bb19a
anaximeno <anaximenobrito@gmail.com>
2022-11-05 18:00:06 -0100

    feat: add main scripting files

---
+++ main.py
@@ -0,0 +1,4 @@
+from teleconverter import Teleconverter
+
+if __name__ == '__main__':
+    Teleconverter().say_hello()
---
+++ teleconverter.py
@@ -0,0 +1,6 @@
+class Teleconverter(object):
+    def __init__(self):
+        super(Teleconverter, self).__init__()
+
+    def say_hello(self):
+        print('Hello from the teleconverter!')
